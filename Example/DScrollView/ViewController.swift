e0fb01dd3f0a8b010b6350a2bb48ec78bb231f9a
Ivan <Ivanlipotan21@gmail.com>
2023-02-21 14:30:34 +0700

    Initial commit

---
+++ Example/DScrollView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DScrollView
+//
+//  Created by 56709569 on 02/21/2023.
+//  Copyright (c) 2023 56709569. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
